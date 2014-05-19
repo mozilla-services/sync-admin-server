2b1703b491e092d274879d365ce0ec246d2b1b2a
Gene Wood <gene_wood@cementhorizon.com>
2014-05-19 15:51:37 -0700

    Fix non absolute path for README.txt

--- SyncNodeManager/setup.py
+++ SyncNodeManager/setup.py
@@ -23,7 +23,7 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 # Get the long description from the relevant file
-with codecs.open('README.txt', encoding='utf-8') as f:
+with codecs.open(os.path.join(here, 'README.txt'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
